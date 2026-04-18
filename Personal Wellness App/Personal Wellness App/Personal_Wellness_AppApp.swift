6190ef67154561ce406af50c6a1a7e06dcb7a16f
Gabriel True <137020604+GabrielETrue@users.noreply.github.com>
2026-04-18 15:55:10 -0400

    Adding xcode project init

---
+++ Personal Wellness App/Personal Wellness App/Personal_Wellness_AppApp.swift
@@ -0,0 +1,32 @@
+//
+//  Personal_Wellness_AppApp.swift
+//  Personal Wellness App
+//
+//  Created by Gabriel True on 4/18/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Personal_Wellness_AppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Personal Wellness App/Personal Wellness AppTests/Personal_Wellness_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Wellness_AppTests.swift
+//  Personal Wellness AppTests
+//
+//  Created by Gabriel True on 4/18/26.
+//
+
+import Testing
+@testable import Personal_Wellness_App
+
+struct Personal_Wellness_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
